61ab1efa522bc498199064759d10f15e51bbb007
ayomideRussianCode <ayindeayomide7@gmail.com>
2024-10-14 00:07:24 +0100

    third commit

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import { Hero } from '../components/Hero/Hero';
+import { Popular } from '../components/popular/Popular';
+import { Offers } from '../components/offers/Offers';
+import { NewCollections } from '../components/new_collections/NewCollections';
+import { NewsLetter } from '../components/news_letter/NewsLetter';
 
 export const Shop = () => {
   return (
     <div>
       <Hero/>
+      <Popular/>
+      <Offers/>
+      <NewCollections/>
+      <NewsLetter/>
     </div>
   )
 }
